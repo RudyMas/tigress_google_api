d7e135883f6876560bb32633c47fb520ed7ff9f7
RudyMasGoNext <rudy.mas@go-next.be>
2024-10-10 14:52:33 +0200

    Added PHPDoc

--- src/GoogleApi.php
+++ src/GoogleApi.php
@@ -2,6 +2,16 @@
 
 namespace Tigress;
 
+/**
+ * Class GoogleApi (PHP version 8.3)
+ *
+ * @author Rudy Mas <rudy.mas@rudymas.be>
+ * @copyright 2024, rudymas.be. (http://www.rudymas.be/)
+ * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
+ * @version 1.0.0
+ * @lastmodified 2024-10-10
+ * @package Tigress\GoogleApi
+ */
 class GoogleApi
 {
     /**
