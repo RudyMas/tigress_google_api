9271915d4a3407df903f83151e07a574aeece24a
RudyMasGoNext <rudy.mas@go-next.be>
2024-10-11 11:36:38 +0200

    Version 2024.10.1

--- src/GoogleApi.php
+++ src/GoogleApi.php
@@ -8,8 +8,8 @@ namespace Tigress;
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2024, rudymas.be. (http://www.rudymas.be/)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 1.0.0
- * @lastmodified 2024-10-10
+ * @version 2024.10.1
+ * @lastmodified 2024-10-11
  * @package Tigress\GoogleApi
  */
 class GoogleApi
@@ -22,7 +22,7 @@ class GoogleApi
     public static function version(): array
     {
         return [
-            'GoogleApi' => '1.0.0',
+            'GoogleApi' => '2024.10.1',
             'GoogleApiAuth' => GoogleApiAuth::version(),
             'GoogleApiCalendar' => GoogleApiCalendar::version(),
             'GoogleApiDrive' => GoogleApiDrive::version(),
